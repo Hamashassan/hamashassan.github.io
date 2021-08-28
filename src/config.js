a0adbe4e6ca3772f8286d48bffe50c57d3305cd0
Hamas Hassan <hamas.hassan1996@gmail.com>
2021-08-28 18:31:52 +0500

    Portfolio Updated
    Portfolio Updated

---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Hamas Hassan | Software Engineer',
+  siteDescription:
+    'Hamas Hassan is an incoming Software Developer, based in Pakistan, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Hamas Hassanb, Chandrika, Deb, chandrikadeb7, software engineer, web developer, javascript, react-native, react, cubix, fiverr, react, developer',
+  siteUrl: 'https://chandrikadeb7.github.io/',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Hamas Hassan',
+  location: 'Karachi, Pakistan',
+  email: 'hamas.hassann@gmail.com',
+  github: 'https://github.com/Hamashassan',
+  // twitterHandle: '@chandrikadeb7',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Hamashassan',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/hamas-hassan/',
+    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/chandrikadeb7',
+    // },
+    // {
+    //   name: 'Instagram',
+    //   url: 'https://www.instagram.com/___cd.___/',
+    // },
+    // {
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/chandrikadeb7',
+    // },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
